3d73aa1d13e1cbdb0b7b6c718d042206f5a6e0e9
nimit0703 <nimit.190410107143@gmail.com>
2023-10-18 15:28:28 +0530

    login first feature added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <template>
       <div class="d-flex align-items-stretch">
         <SidebarComponent class="border-right"></SidebarComponent>
+        <BeforeLogin v-show="(!isLogin && $route.path !== '/login')"></BeforeLogin>
         <router-view class="p-3 flex-grow-1"></router-view>
         <SpinnerCom v-show="showSpin"></SpinnerCom>
       </div>
@@ -13,16 +14,21 @@
 <script lang="ts">
 import SidebarComponent from "./components/SidebarCom.vue";
 import SpinnerCom from "./components/SpinnerCom.vue";
+import BeforeLogin from "./components/BeforeLogin.vue";
 export default {
   name: "App",
   components: { 
     SidebarComponent, 
-    SpinnerCom 
+    SpinnerCom,
+    BeforeLogin,
   },
   computed: {
     showSpin() {
       return this.$store.state.showSpinner;
     },
+    isLogin(){
+      return this.$store.state.isLogin;
+    }
   },
 };
 </script>
---
+++ src/components/BeforeLogin.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="beforeLogin">
+    <div class="beforeLogin-content">
+      <p>Login First</p>
+      <router-link to="login">Login</router-link>
+      <p>Current Route: {{ $route.path }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.beforeLogin {
+  position: fixed;
+  background-color: hsla(0, 0%, 0%, 0.342);
+  top: 0;
+  left: 20vw;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  backdrop-filter: blur(5px);
+  color: #fff;
+}
+.beforeLogin-content {
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 30vh;
+  left: 30vw;
+  align-items: center;
+  font-size: 2vw;
+}
+.beforeLogin-content p {
+  font-size: 3vw;
+}
+</style>
